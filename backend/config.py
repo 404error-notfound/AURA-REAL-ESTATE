5ee40169d7043a78d23582af3ad03937c03f1a92
Angela Omondi <omondiangela426@gmail.com>
2025-08-19 23:49:14 +0300

    database password changed

--- backend/config.py
+++ backend/config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL",
-        "postgresql://postgres:wh@tdoyoUth!nk?@localhost/aurare"
+        "postgresql://postgres:whatdoyouthink@localhost/aurare"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
